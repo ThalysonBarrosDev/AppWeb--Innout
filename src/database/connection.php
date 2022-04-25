1bed6c6a4bfa835e159e77498621f91d395ed667
Thalyson <thalysonbarrospereira2019@gmail.com>
2022-04-25 20:23:52 -0300

    Adicionando projeto ao repositorio remoto.

---
+++ src/database/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Database {
+
+        public static function getConnection() {
+
+            $env_path = realpath(dirname(__FILE__) . '/../env.ini');
+            $env = parse_ini_file($env_path);
+
+            $conn = new mysqli($env['host'], $env['username'], $env['password'], $env['database']);
+
+            if ($conn->connect_error) {
+
+                die ("Erro: " . $conn->connect_error);
+
+            }
+
+            return $conn;
+        }
+
+    }
