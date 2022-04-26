96838651459dfe23bf081b06254edaee42a0a3ae
Thalyson <thalysonbarrospereira2019@gmail.com>
2022-04-26 15:02:38 -0300

    Aplicando CSS na tela de batimento de ponto

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,3 +1,30 @@
 <main class="content">
-    
+    <div class="content-title mb-4"><i class="icon icofont-check-alt mr-2"></i></div>
+
+    <div>
+        <h1>Registrar Ponto</h1>
+        <h2>Mantenha seu ponto consistente!</h2>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h3>10 de Janeiro de 2022</h3>
+            <p class="mb-0">Os batimentos efetuados hoje</p>
+        </div>
+
+        <div class="card-body">
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 1: ---</span>
+                <span>Saída 1: ---</span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 2: ---</span>
+                <span>Saída 2: ---</span>
+            </div>
+        </div>
+
+        <div class="card-footer d-flex justify-content-center">
+            <a href="???" class="btn btn-success btn-lg"><i class="icofont-check mr-1"></i>Bater Ponto</a>
+        </div>
+    </div>
 </main>
