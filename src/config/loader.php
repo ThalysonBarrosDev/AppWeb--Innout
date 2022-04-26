6528ad60e146870a944984cdb904d64d2d2cf279
Thalyson <thalysonbarrospereira2019@gmail.com>
2022-04-26 14:20:10 -0300

    Aplicando CSS no header

--- src/config/loader.php
+++ src/config/loader.php
@@ -43,6 +43,7 @@
         }
  
         require_once (TEMPLATE_PATH . "/header.php");
+        require_once (TEMPLATE_PATH . "/left.php");
         require_once (VIEW_PATH . "/{$viewname}.php");
         require_once (TEMPLATE_PATH . "/footer.php");
 
--- src/views/template/header.php
+++ src/views/template/header.php
@@ -12,7 +12,6 @@
 </head>
 
 <body>
-
     <header class="header">
         <div class="logo">
             <i class="icofont-travelling mr-2"></i>
@@ -24,7 +23,20 @@
             <i class="icofont-navigation-menu"></i>
         </div>
 
-        <div class="spacer">
-            
+        <div class="spacer"></div>
+
+        <div class="dropdown">
+            <div class="dropdown-button">
+                <span class="ml-3">Usuário Mock</span>
+                <i class="icofont-simple-down mx-2"></i>
+            </div>
+
+            <div class="dropdown-content">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="logout.php"><i class="icofont-logout mr-2"></i>Sair</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </header>
